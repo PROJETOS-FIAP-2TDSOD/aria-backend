b0c69c1e0654cbd5e808c6053b76ad01068a220a
Bruno Queiroz Pires <brunoqpires@gmail.com>
2026-09-12 19:51:02 -0300

    chore: estrutura inicial do projeto Spring Boot

---
+++ src/main/java/com/fiap/aria_backend/AriaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.aria_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AriaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AriaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/aria_backend/AriaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.aria_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AriaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
