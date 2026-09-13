322b26e8a57a1c20d569a74fbc3f1bebdab7cb70
Bruno Queiroz Pires <brunoqpires@gmail.com>
2026-09-13 15:39:15 -0300

    feat: cria entidade Notification, enum de tipos e repositorio

---
+++ src/main/java/com/fiap/aria_backend/model/Notification.java
@@ -0,0 +1,36 @@
+package com.fiap.aria_backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "notifications")
+public class Notification {
+
+    @Id
+    private String id;
+
+    private String userId;
+
+    private String title;
+
+    private String message;
+
+    private NotificationType type;
+
+    @Builder.Default
+    private boolean isRead = false;
+
+    private String relatedIdeaId;
+
+    private LocalDateTime createdAt;
+}
