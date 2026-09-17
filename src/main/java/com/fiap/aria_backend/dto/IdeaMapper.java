7e0d06b6ca6e5f13656bbd2a85cb1e79a6b69685
Bruno Queiroz Pires <brunoqpires@gmail.com>
2026-09-16 21:23:20 -0300

    feat: expoe pontos e badges no resumo do usuario

--- src/main/java/com/fiap/aria_backend/dto/IdeaMapper.java
+++ src/main/java/com/fiap/aria_backend/dto/IdeaMapper.java
@@ -1,11 +1,24 @@
 package com.fiap.aria_backend.dto;
 
 import com.fiap.aria_backend.model.Idea;
+import com.fiap.aria_backend.model.IdeaStatus;
 import com.fiap.aria_backend.model.User;
+import com.fiap.aria_backend.repository.IdeaRepository;
+import com.fiap.aria_backend.service.GamificationService;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class IdeaMapper {
+
+    private final IdeaRepository ideaRepository;
+    private final GamificationService gamificationService;
+
+    public IdeaMapper(IdeaRepository ideaRepository, GamificationService gamificationService) {
+        this.ideaRepository = ideaRepository;
+        this.gamificationService = gamificationService;
+    }
 
     public IdeaResponseDto toResponseDto(Idea idea, User author) {
         return IdeaResponseDto.builder()
@@ -29,7 +42,11 @@ public class IdeaMapper {
                 .build();
     }
 
-    private UserSummaryDto toUserSummary(User user) {
+    // Publico: tambem usado pelo AuthService para montar o resumo do usuario no login/registro
+    public UserSummaryDto toUserSummary(User user) {
+        List<Idea> ideas = ideaRepository.findByAuthorId(user.getId());
+        int approvedIdeas = (int) ideas.stream().filter(i -> i.getStatus() == IdeaStatus.APROVADA).count();
+
         return UserSummaryDto.builder()
                 .id(user.getId())
                 .name(user.getName())
@@ -37,8 +54,10 @@ public class IdeaMapper {
                 .role(user.getRole().name())
                 .department(user.getDepartment())
                 .avatarInitials(user.getAvatarInitials())
-                .totalIdeas(0)   // TODO: calcular quando a gamificação for implementada
-                .approvedIdeas(0) // TODO: idem
+                .totalIdeas(ideas.size())
+                .approvedIdeas(approvedIdeas)
+                .points(gamificationService.calculatePoints(ideas))
+                .badges(gamificationService.calculateBadgeIds(ideas))
                 .build();
     }
 }
